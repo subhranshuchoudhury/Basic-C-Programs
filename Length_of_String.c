6f5b3dafcf00684d5c801cf4d8880d94bc814baf
SUBHRANSHU SEKHAR CHOUDHURY <63858190+SubhranshuSekharChoudhury@users.noreply.github.com>
2022-01-04 19:58:57 +0530

    Update Length_of_String.c

--- Length_of_String.c
+++ Length_of_String.c
@@ -7,8 +7,16 @@ int main()
     gets(name); //this will allow to store full word.
     while(name[i]!='\0')//bcz the last index is inputed "\0" by self computer.
     {
+        if(name[i]==' ')
+        {
+            j++;
+            i++;
+        }
+        else
         i++;
     }
+    
+    i-=j;
     
     printf("Total length is %d.",i);
     return 0;

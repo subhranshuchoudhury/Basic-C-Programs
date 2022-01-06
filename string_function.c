2c26a1bf34d53b6fc17a57df0c729ffd620d08ac
Subhranshu Choudhury <63858190+PunishTheEvil@users.noreply.github.com>
2022-01-06 13:29:14 +0530

    Update string_function.c

--- string_function.c
+++ string_function.c
@@ -3,10 +3,18 @@
 
 int main()
 {
-    char input[50],input2[50];
+    char input[50],input2[50],savestring[50];
+    int i;
     
     printf("Enter 1st String: ");
     gets(input);
+    //save the 1st string bcz we use strcpy before strcmp. 
+    //do this or you can just write strcpy after strcmp :)
+    
+    for(i=0;input[i]!='\0';i++)
+    savestring[i]=input[i];
+    savestring[i]='\0';//manually add null in last index of the string.
+    
     printf("Enter 2nd String: ");
     gets(input2);
     printf("String Functions Are: \n");
@@ -18,18 +26,17 @@ int main()
     strcat(input,input2);
     printf("\nstring concatenate:%s",input);
     //strcpy()
+    
     strcpy(input,input2);
     printf("\nstring copied from input2:%s",input);
-    //strcmp()
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //strcmp() - string compare.
+    //returns integer values.
+    if(strcmp(savestring,input2)==0)
+    printf("\nstring are equal.");
+    else if(strcmp(savestring,input2)>0)
+    printf("\n1st string is larger.");
+    else if(strcmp(savestring,input2)<0)
+    printf("\n2nd string is larger.");
 
     return 0;
 }

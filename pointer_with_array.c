a3f5e4e1534c1bc31955d56dd317956b376d3db2
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-11 20:22:40 +0530

    Update pointer_with_array.c

--- pointer_with_array.c
+++ pointer_with_array.c
@@ -2,21 +2,23 @@
 //asign 1st address of the array to pointer.
 //eg. p = a[0]; or p = a;
 //array- continous memory allocation.
-
+#include <stdio.h>
 int main()
 {
-    int store[10],i,*p;
-    for(i=0;i<10;i++)
+    int store[5],i,*p;
+    
+    for(i=0;i<5;i++)
     {
         printf("Enter Number: ");
         scanf("%d",&store[i]);
     }
     
     //connecting the 1st address to pointer.
-    p = store[0];
+    p = &store[0]; //[0] is the 1st address.
     
-    int store[10],i,*p;
-    for(i=0;i<10;i++)
+    
+    printf("Pointer Value: ");
+    for(i=0;i<5;i++)
     {
         
         printf("%d",*(p+i));

95e2273b43e9520a57f946ae88e893a1f1e92707
Subhranshu Choudhury <63858190+subhranshuchoudhury@users.noreply.github.com>
2022-01-14 10:53:43 +0530

    Update filehandling_fgetc().c

--- filehandling_fgetc().c
+++ filehandling_fgetc().c
@@ -3,12 +3,16 @@
 int main() {
   char c;
   FILE *fh;//declare file handler.
+  if(fh==NULL)
+    printf("File not exist. Pls create the file.");
   //create a file "test.txt" in compiler directory.
+  //add a simple word in that file. eg.good
   //simply use online compiler (gdb c compiler).
-  fh = fopen("test.txt","r");//r-open in read mode.
+  fh = fopen("test.txt","r");//r-open in read mode. NOTE: Must Create a file names "test.txt".
   while((c=fgetc(fh))!=EOF)
   {
-    printf("The Get Value: %c\n",c);
+    printf("%c",c);
+    //it will print the text inside the "test.txt" file.
   }
   fclose(fh);//close the open file.
   return 0;
